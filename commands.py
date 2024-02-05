c7da78bcecda906ba2d9233c6f3c55c481323469
Pavel <rudnikcode@gmail.com>
2024-02-05 19:56:08 +1000

    pyautogui commands volumemute and playpause

--- commands.py
+++ commands.py
@@ -1,9 +1,20 @@
 import keyboard
+import pyautogui
 import os
-
-#keyboard.write("Hello")
 
 
 def cmd_command(command):
     os.system(command)
 
+
+def click_key(btn):
+    keyboard.send(btn)
+
+
+def do_action(action):
+    pyautogui.press(action)
+
+
+# 'volumedown', 'volumemute', 'volumeup', "playpause", 'prevtrack', 'nexttrack'
+# https://pyautogui.readthedocs.io/en/latest/keyboard.html
+pyautogui.press('prevtrack')
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import telebot
 from telebot import types
 import const
 from rw_config import *
+from commands import *
 
 token = 'token'
 bot = telebot.TeleBot(token)
@@ -9,7 +10,7 @@ bot = telebot.TeleBot(token)
 
 @bot.message_handler(commands=['start', 'help'])
 def start_message(message):
-    bot.send_message(message.chat.id, const.list_com + get_commands().join("\n"))
+    bot.send_message(message.chat.id, const.list_com)  #+ get_commands().join("\n"))
 
 
 @bot.message_handler(commands=['add'])
@@ -17,6 +18,36 @@ def button_message(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     item1 = types.KeyboardButton("Кнопка")
     markup.add(item1)
+
+
+@bot.message_handler(commands=['playpause'])
+def play_pause(message):
+    do_action("playpause")
+
+
+@bot.message_handler(commands=['next'])
+def play_next(message):
+    pass
+
+
+@bot.message_handler(commands=['previous'])
+def play_previous(message):
+    pass
+
+
+@bot.message_handler(commands=['volumeup'])
+def volume_up(message):
+    pass
+
+
+@bot.message_handler(commands=['volumedown'])
+def volume_down(message):
+    pass
+
+
+@bot.message_handler(commands=['volumemute'])
+def volume_mute(message):
+    do_action("volumemute")
 
 
 bot.infinity_polling()
